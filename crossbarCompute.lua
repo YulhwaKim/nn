8409c7cca9a6c7155be4148be35593c3665a5318
Yulhwa Kim <35885699+YulhwaKim@users.noreply.github.com>
2018-08-10 10:10:26 +0900

    Create crossbarCompute.lua

---
+++ crossbarCompute.lua
@@ -0,0 +1,70 @@
+local THNN = require 'nn.THNN'
+local crossbarCompute, parent = torch.class('nn.crossbarCompute', 'nn.Linear')
+
+
+function crossbarCompute:__init(inputSize, outputSize)
+   local delayedReset = self.reset
+   self.reset = function() end
+   parent.__init(self, inputSize, outputSize)
+   self.reset = delayedReset
+   self.weight = torch.Tensor(outputSize, inputSize)
+   self.weightB = torch.Tensor(outputSize, inputSize)
+   self.weightOrg = torch.Tensor(outputSize, inputSize)
+   self.bias = torch.Tensor(outputSize)
+   self.gradWeight = torch.Tensor(outputSize, inputSize)
+   self.gradBias = torch.Tensor(outputSize)
+   self.accumN = inputSize
+   self:reset()
+   -- should nil for serialization, the reset will still work
+   self.reset = nil
+end
+
+function crossbarCompute:reset(stdv)
+   if stdv then
+      stdv = stdv * math.sqrt(3)
+   else
+      stdv = 1./math.sqrt(self.weight:size(2))
+   end
+   if nn.oldSeed then
+      for i=1,self.weight:size(1) do
+         self.weight:select(1, i):apply(function()
+            return torch.uniform(-1, 1)
+         end)
+         self.bias[i] = torch.uniform(-stdv, stdv)
+      end
+   else
+      self.weight:uniform(-1, 1)
+      self.bias:uniform(-stdv, stdv)
+   end
+
+   return self
+end
+
+
+function crossbarCompute:binarized()
+	self.weightOrg:copy(self.weight)
+	self.weightB:copy(self.weight):add(1):div(2):clamp(0,1)
+	self.weightB:round():mul(2):add(-1)
+	return  self.weightB
+end
+
+function crossbarCompute:updateOutput(input)
+	-- get binary weight
+	self.weightB = self:binarized()
+	self.weight:copy(self.weightB)
+	-- update Output
+	input.THNN.crossbarCompute_updateOutput(
+		self.output:cdata(),
+		input:cdata(),
+		self.weight:cdata(),
+		self.accumN)
+	-- restore original weight
+	self.weight:copy(self.weightOrg);
+	return self.output
+end
+
+
+function crossbarCompute:__tostring__()
+  return torch.type(self) ..
+      string.format('(%d -> %d)', self.weight:size(2), self.weight:size(1))
+end

116fc6217205f68a28d9894240401fc566dc8bba
Yulhwa Kim <35885699+YulhwaKim@users.noreply.github.com>
2018-08-19 07:51:24 +0900

    Update CrossbarSpatialConvolution.lua

--- CrossbarSpatialConvolution.lua
+++ CrossbarSpatialConvolution.lua
@@ -1,7 +1,7 @@
 local THNN = require 'nn.THNN'
 local CrossbarSpatialConvolution, parent = torch.class('nn.CrossbarSpatialConvolution', 'nn.Module')
 
-function SpatialConvolutionMM:__init(nInputPlane, nOutputPlane, kW, kH, dW, dH, padW, padH)
+function CrossbarSpatialConvolution:__init(nInputPlane, nOutputPlane, kW, kH, dW, dH, padW, padH, accumN)
    parent.__init(self)
 
    dW = dW or 1
@@ -18,9 +18,80 @@ function SpatialConvolutionMM:__init(nInputPlane, nOutputPlane, kW, kH, dW, dH, 
    self.padH = padH or self.padW
 
    self.weight = torch.Tensor(nOutputPlane, nInputPlane*kH*kW)
-   self.bias = torch.Tensor(nOutputPlane)
-   self.gradWeight = torch.Tensor(nOutputPlane, nInputPlane*kH*kW)
-   self.gradBias = torch.Tensor(nOutputPlane)
-
+   self.weightB = torch.Tensor(nOutputPlane, nInputPlane*kH*kW)
+   self.weightOrg = torch.Tensor(nOutputPlane, nInputPlane*kH*kW)
+   self.accumN = accumN or inputSize
+   
+   self.binarize = true
+   
    self:reset()
+end
+
+
+function CrossbarSpatialConvolution:reset(stdv)
+   if stdv then
+      stdv = stdv * math.sqrt(3)
+   else
+      stdv = 1/math.sqrt(self.kW*self.kH*self.nInputPlane)
+   end
+   if nn.oldSeed then
+      self.weight:apply(function()
+         return torch.uniform(-stdv, stdv)
+      end)
+   else
+      self.weight:uniform(-stdv, stdv)
+   end
+end
+
+
+function CrossbarSpatialConvolution:binarized()
+	self.weightOrg:copy(self.weight)
+	self.weightB:copy(self.weight):add(1):div(2):clamp(0,1)
+	self.weightB:round():mul(2):add(-1)
+	return  self.weightB
+end
+
+
+function CrossbarSpatialConvolution:updateOutput(input)
+   -- get binary weight
+	if self.binarize == true then
+		self.weightB = self:binarized()
+		self.weight:copy(self.weightB)
+	end
+   
+   assert(input.THNN, torch.type(input)..'.THNN backend not imported')
+   self.finput = self.finput or input.new()
+   self.fgradInput = self.fgradInput or input.new()
+   -- backward compatibility
+   if self.padding then
+      self.padW = self.padding
+      self.padH = self.padding
+      self.padding = nil
+   end
+   -- update output
+   input.THNN.CrossbarSpatialConvolution_updateOutput(
+      input:cdata(),
+      self.output:cdata(),
+      self.weight:cdata(),
+      self.finput:cdata(),
+      accumN,
+      self.kW, self.kH,
+      self.dW, self.dH,
+      self.padW, self.padH
+   )
+   return self.output
+end
+
+
+
+function CrossbarSpatialConvolution:__tostring__()
+   local s = string.format('%s(%d -> %d, %dx%d', torch.type(self),
+         self.nInputPlane, self.nOutputPlane, self.kW, self.kH)
+   if self.dW ~= 1 or self.dH ~= 1 or self.padW ~= 0 or self.padH ~= 0 then
+     s = s .. string.format(', %d,%d', self.dW, self.dH)
+   end
+   if (self.padW or self.padH) and (self.padW ~= 0 or self.padH ~= 0) then
+     s = s .. ', ' .. self.padW .. ',' .. self.padH
+   end
+   return s .. ')'
 end

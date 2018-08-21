b52b3ba67a3f318879715f0ff09f7c68959372b0
Yulhwa Kim <35885699+YulhwaKim@users.noreply.github.com>
2018-08-21 15:18:53 +0900

    Update CrossbarSpatialConvolution.c

--- lib/THNN/generic/CrossbarSpatialConvolution.c
+++ lib/THNN/generic/CrossbarSpatialConvolution.c
@@ -107,8 +107,9 @@ static void THNN_(CrossbarSpatialConvolution_updateOutput_frame)(
         real temp = 0;
         for (int n=0; n<accumN; n++) {
           temp += finput_real[(k*accumN+n)*nOutSpatial+j] * weight_real[i*nIn+(k*accumN+n)];
-          printf("idx nOutputPlane: %ld, idx nIn: %ld, nIn: %ld, idx linear: %ld,  weight: %.1f\n", 
-                 i, (k*accumN+n), nIn, i*nIn+(k*accumN+n), weight_real[i*nIn+(k*accumN+n)]);
+          // debug weight index
+//           printf("idx nOutputPlane: %ld, idx nIn: %ld, nIn: %ld, idx linear: %ld,  weight: %.1f\n", 
+//                  i, (k*accumN+n), nIn, i*nIn+(k*accumN+n), weight_real[i*nIn+(k*accumN+n)]);
 //           printf("accumN: %d, n: %d, finput_real: %.1f, weight_real: %.1f, temp: %.1f\n", 
 //                  accumN, n, finput_real[(k*accumN+n)*nOutSpatial+j],weight_real[i*nIn+(k*accumN+n)], temp);
         }

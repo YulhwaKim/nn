20e34700429b5461c81c756802b7b9700ca668d3
Yulhwa Kim <35885699+YulhwaKim@users.noreply.github.com>
2018-08-24 13:02:06 +0900

    Update CrossbarLinearWvar.lua

--- CrossbarLinearWvar.lua
+++ CrossbarLinearWvar.lua
@@ -1,28 +1,22 @@
 local THNN = require 'nn.THNN'
-local CrossbarCompute, parent = torch.class('nn.CrossbarCompute', 'nn.Module')
+local CrossbarLinearWvar, parent = torch.class('nn.CrossbarLinearWvar', 'nn.Module')
 
 
-function CrossbarCompute:__init(inputSize, outputSize, accumN, binarize)
+function CrossbarLinearWvar:__init(inputSize, outputSize, accumN, binarize)
    local delayedReset = self.reset
    self.reset = function() end
    parent.__init(self, inputSize, outputSize)
    self.reset = delayedReset
    self.weight = torch.Tensor(outputSize, inputSize)
-   self.weightB = torch.Tensor(outputSize, inputSize)
-   self.weightOrg = torch.Tensor(outputSize, inputSize)
-   self.gradWeight = torch.Tensor(outputSize, inputSize)
-   self.gradBias = torch.Tensor(outputSize)
+   self.VarP = torch.Tensor(outputSize, inputSize)
+   self.VarM = torch.Tensor(outputSize, inputSize)
    self.accumN = accumN or inputSize
-   self.binarize = binarize or false
-	if (binarize and type(self.binarize) ~= 'boolean') then
-		error('binarize flag must be boolean')
-	end
    self:reset()
    -- should nil for serialization, the reset will still work
    self.reset = nil
 end
 
-function CrossbarCompute:reset(stdv)
+function CrossbarLinearWvar:reset(stdv)
    if stdv then
       stdv = stdv * math.sqrt(3)
    else
@@ -41,35 +35,20 @@ function CrossbarCompute:reset(stdv)
    return self
 end
 
-
-function CrossbarCompute:binarized()
-	self.weightOrg:copy(self.weight)
-	self.weightB:copy(self.weight):add(1):div(2):clamp(0,1)
-	self.weightB:round():mul(2):add(-1)
-	return  self.weightB
-end
-
-function CrossbarCompute:updateOutput(input)
-	-- get binary weight
-	if self.binarize == true then
-		self.weightB = self:binarized()
-		self.weight:copy(self.weightB)
-	end
+function CrossbarLinearWvar:updateOutput(input)
 	-- update Output
-	input.THNN.CrossbarCompute_updateOutput(
+	input.THNN.CrossbarLinearWvar_updateOutput(
 		self.output:cdata(),
 		input:cdata(),
 		self.weight:cdata(),
+		self.VarP:cdata(),
+		self.VarM:cdata(),
 		self.accumN)
-	-- restore original weight
-	if self.binarize == true then
-		self.weight:copy(self.weightOrg);
-	end
 	return self.output
 end
 
 
-function CrossbarCompute:__tostring__()
+function CrossbarLinearWvar:__tostring__()
   return torch.type(self) ..
       string.format('(%d -> %d)', self.weight:size(2), self.weight:size(1))
 end
